55a9b272b2ca888194121387c5cecd126beea046
lethurh <135083340+lethurh@users.noreply.github.com>
2024-07-20 15:18:55 +0200

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,8 +0,0 @@
-# this does something
-file {'/var/www/html/wp-settings.php':
-  ensure => present,
-}-> exec { 'fix a line in wp-settings.php':
-  path    => '/usr/local/bin/:/bin/',
-  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' \
-  /var/www/html/wp-settings.php",
-}
