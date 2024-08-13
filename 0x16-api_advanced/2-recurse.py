e27b011881c1aedd6d95f3411b2e5c4779c6571e
lethurh <gugulethupraxedis@gmail.com>
2024-08-13 19:49:18 +0000

    2. Recurse it!

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""""advanced api oth tasks"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """"Alx verification of editing tools"""
+    headers = {'User-Agent': 'Linux/client/0.0'}
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
+    response = requests.get(url, headers=headers)
+    if response.status_code == 404:
+        return None
+    result = response.json()['data']['children']
+    max_count = len(result)
+    count = len(hot_list)
+    if hot_list == []:
+        count = 0
+    if count < max_count:
+        hot_list.append(result[count])
+        recurse(subreddit,hot_list)
+    return hot_list
