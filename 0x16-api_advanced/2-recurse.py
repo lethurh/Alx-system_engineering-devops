8d6c59314799b8e7c0cf3dd82c5ad828f90854be
lethurh <gugulethupraxedis@gmail.com>
2024-08-14 08:08:24 +0000

    2. Recurse it!

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-""""advanced api oth tasks"""
-import requests
+"""Module for task 2"""
 
 
-def recurse(subreddit, hot_list=[]):
-    """"Alx verification of editing tools"""
-    headers = {'User-Agent': 'Linux/client/0.0'}
-    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
-    response = requests.get(url, headers=headers)
-    if response.status_code == 404:
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """Queries the Reddit API and returns all hot posts
+    of the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                            .format(subreddit),
+                            params={"count": count, "after": after},
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 400:
         return None
-    result = response.json()['data']['children']
-    max_count = len(result)
-    count = len(hot_list)
-    if hot_list == []:
-        count = 0
-    if count < max_count:
-        hot_list.append(result[count])
-        recurse(subreddit,hot_list)
-    return hot_list
+
+    hot_l = hot_list + [child.get("data").get("title")
+                        for child in sub_info.json()
+                        .get("data")
+                        .get("children")]
+
+    info = sub_info.json()
+    if not info.get("data").get("after"):
+        return hot_l
+
+    return recurse(subreddit, hot_l, info.get("data").get("count"),
+                   info.get("data").get("after"))
