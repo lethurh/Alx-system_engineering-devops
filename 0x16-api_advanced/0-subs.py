7a14f1c45b812c6d31e2bf3457e0f3f1a8413091
lethurh <gugulethupraxedis@gmail.com>
2024-08-14 08:20:48 +0000

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-""""advanced api oth tasks"""
-import requests
+"""Module for task 0"""
 
 
 def number_of_subscribers(subreddit):
-    headers = {'User-Agent': 'Linux/client/0.0'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = requests.get(url, headers=headers)
-    if response.status_code == 404:
+    """Queries the Reddit API and returns the number of subscribers
+    to the subreddit"""
+    import requests
+
+    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
         return 0
-    else:
-        subsciber = response.json()['data']['subscribers']
-        return subsciber
+
+    return sub_info.json().get("data").get("subscribers")
